add4208d994b6e16ee414497a198b0b0d0734acb
Alex <az.nov2012@gmail.com>
2017-01-31 14:56:26 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.currGuess = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange(_min, _max) {
+    	this.min = _min;
+    	this.max = _max;
     }
 
     guess() {
-
+    	return this.currGuess =  Math.round((this.min + this.max)/2);    	
     }
 
     lower() {
-
+    	this.max = this.currGuess;
     }
 
     greater() {
-
+    	this.min = this.currGuess;
     }
 }
 
